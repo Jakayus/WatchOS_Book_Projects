b4ce68ad71a500c0058d6869497ba46e18c941bb
Jakayus <jakayus123@gmail.com>
2021-11-21 23:31:18 -0700

    Starting point for Project 2
    Signed-off-by: Jakayus <jakayus123@gmail.com>

---
+++ Project2/Project2 WatchKit Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Project2 WatchKit Extension
+//
+//  Created by Joel Sereno on 11/21/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Project2/Project2 WatchKit Extension/Project2App.swift
@@ -0,0 +1,19 @@
+//
+//  Project2App.swift
+//  Project2 WatchKit Extension
+//
+//  Created by Joel Sereno on 11/21/21.
+//
+
+import SwiftUI
+
+@main
+struct Project2App: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
