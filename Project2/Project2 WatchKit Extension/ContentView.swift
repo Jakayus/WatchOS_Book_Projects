4c44f54e58ce5d03985364bcc1e89d199afbfb1c
Jakayus <jakayus123@gmail.com>
2021-11-22 23:45:15 -0700

    Finish creating initial layout.
    Signed-off-by: Jakayus <jakayus123@gmail.com>

--- Project2/Project2 WatchKit Extension/ContentView.swift
+++ Project2/Project2 WatchKit Extension/ContentView.swift
@@ -16,11 +16,29 @@ struct ContentView: View {
     @State private var question = "rock"
     @State private var title = "Win"
     
+    let moves = ["rock", "paper", "scissors"]
+    
     var body: some View {
         VStack {
             Image(question)
+                .resizable()
+                .scaledToFit()
             Divider()
                 .padding(.vertical)
+            //HStack for 3 buttons
+            HStack{
+                ForEach(moves, id: \.self){ type in
+                    Button {
+                        select(move: type)
+                    } label: {
+                        Image(type)
+                            .resizable()
+                            .scaledToFit()
+                    }
+                    .buttonStyle(PlainButtonStyle())
+                    
+                }
+            }
         }
         .navigationTitle(title)
     }
