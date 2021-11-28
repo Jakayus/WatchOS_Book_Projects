b231bcdbbb95403956ee12c27e457d06eae36a5f
Joel Sereno <jakayus123@gmail.com>
2021-11-27 19:18:46 -0700

    Create starting point for Project4 (default Watch project)
    Signed-off-by: Joel Sereno <jakayus123@gmail.com>

---
+++ Project4/Project4 WatchKit Extension/Project4App.swift
@@ -0,0 +1,19 @@
+//
+//  Project4App.swift
+//  Project4 WatchKit Extension
+//
+//  Created by Joel Sereno on 11/27/21.
+//
+
+import SwiftUI
+
+@main
+struct Project4App: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
