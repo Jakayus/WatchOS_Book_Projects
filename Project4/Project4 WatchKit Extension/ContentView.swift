4b9b38f7db37cace857a764d5d9a44dfcf81e6b2
Joel Sereno <jakayus123@gmail.com>
2021-11-28 21:59:02 -0700

    Create picker view for currencies
    Signed-off-by: Joel Sereno <jakayus123@gmail.com>

--- Project4/Project4 WatchKit Extension/ContentView.swift
+++ Project4/Project4 WatchKit Extension/ContentView.swift
@@ -9,6 +9,9 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var amount = 500.0
+    @State private var selectedCurrency = "USD"
+    
+    static let currencies = ["USD", "AUD", "CAD", "CHF", "CNY", "EUR", "GBP", "HKD", "JPY", "SGD"] //static will allow the array to be used elsewhere in the app
     
     var body: some View {
         VStack {
@@ -19,7 +22,21 @@ struct ContentView: View {
             //1. move between 0 and 1000 inclusive
             //2. sets the step count to 20 (up/down by 20)
             //3. two way binding for amount property
-        }
+            Picker("Select a currency", selection: $selectedCurrency) {
+                ForEach(Self.currencies, id: \.self) { currency in
+                    Text(currency)
+                }
+            }//end Picker
+            .labelsHidden()
+            
+            //Picker notes
+            //1. two way binding to selectedCurrency
+            //2. ForEach loops over all items in currencies to create items. Self refers to static property currencies
+            //3. each item looped is converted into a TextView
+            //4. modifier hides picker label
+            
+        }//end VStack
+        
     }
 }
 
