154bb08fa4b37a5e14bc835785b3a07a7fc96a99
Joel Sereno <jakayus123@gmail.com>
2021-11-29 23:30:58 -0700

    Update UI Note - there appears to be a UI bug, need to fix
    Also updated project settings in order to deploy to device
    
    Signed-off-by: Joel Sereno <jakayus123@gmail.com>

--- Project4/Project4 WatchKit Extension/ContentView.swift
+++ Project4/Project4 WatchKit Extension/ContentView.swift
@@ -14,36 +14,48 @@ struct ContentView: View {
     static let currencies = ["USD", "AUD", "CAD", "CHF", "CNY", "EUR", "GBP", "HKD", "JPY", "SGD"] //static will allow the array to be used elsewhere in the app
     
     var body: some View {
-        VStack {
-            Text("\(Int(amount))")
-                .font(.system(size: 52))
-            Slider(value: $amount, in: 0...1000, step: 20)
-            //Slider notes
-            //1. move between 0 and 1000 inclusive
-            //2. sets the step count to 20 (up/down by 20)
-            //3. two way binding for amount property
-            HStack {
-                Picker("Select a currency", selection: $selectedCurrency) {
-                    ForEach(Self.currencies, id: \.self) { currency in
-                        Text(currency)
-                    }
-                }//end Picker
-                .labelsHidden()
-                
-                NavigationLink(destination: Text("Detail View")) {
-                    Text("Go")
-                }
-            }
-            
-            //Picker notes
-            //1. two way binding to selectedCurrency
-            //2. ForEach loops over all items in currencies to create items. Self refers to static property currencies
-            //3. each item looped is converted into a TextView
-            //4. modifier hides picker label
-            
-        }//end VStack
         
-    }
+        GeometryReader { geo in
+            VStack (spacing: 0) {
+                Text("\(Int(amount))")
+                    .font(.system(size: 52))
+                    .frame(height: geo.size.height / 3)
+                
+                Slider(value: $amount, in: 0...1000, step: 20)
+                    .accentColor(.green)
+                    .frame(height: geo.size.height / 3)
+                //Slider notes
+                //1. move between 0 and 1000 inclusive
+                //2. sets the step count to 20 (up/down by 20)
+                //3. two way binding for amount property
+                HStack {
+                    Picker("Select a currency", selection: $selectedCurrency) {
+                        ForEach(Self.currencies, id: \.self) { currency in
+                            Text(currency)
+                        }
+                    }//end Picker
+                    .labelsHidden()
+                    
+                    NavigationLink(destination: Text("Detail View")) {
+                        Text("Go")
+                    }
+                    .frame(width: geo.size.width * 0.4) //40% of available width
+                }
+                .frame(height: geo.size.height / 3)
+                
+                //Picker notes
+                //1. two way binding to selectedCurrency
+                //2. ForEach loops over all items in currencies to create items. Self refers to static property currencies
+                //3. each item looped is converted into a TextView
+                //4. modifier hides picker label
+                
+            }//end VStack
+            .navigationTitle("WatchFX")
+            
+        }//end Geometry Reader
+    }//end View
+    
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
