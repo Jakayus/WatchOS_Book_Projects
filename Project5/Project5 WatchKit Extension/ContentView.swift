dfff947ae8501c332bfdeb8a81f335da0501510f
Joel Sereno <jakayus123@gmail.com>
2021-12-13 08:50:55 -0700

    Add Color definitions
    Add Color dictionary which is then used by a function producing a Text View by interpreting color keys

--- Project5/Project5 WatchKit Extension/ContentView.swift
+++ Project5/Project5 WatchKit Extension/ContentView.swift
@@ -8,9 +8,31 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    //MARK: - Properties
+    let colors = [
+        "Red": Color(red:1, green: 0, blue: 0),
+        "Green": Color(red: 0, green: 0.5, blue: 0),
+        "Blue": Color(red: 0, green: 0, blue: 1),
+        "Orange": Color(red: 1, green: 0.6, blue: 0),
+        "Purple": Color(red: 0.5, green: 0, blue: 0.5),
+        "Black" : Color.black
+    ]
+    
+    @State private var colorKeys = ["Red", "Green", "Blue", "Orange", "Purple", "Black"]
+    
+    //MARK: - View
     var body: some View {
         Text("Hello, World!")
             .padding()
+    }
+    
+    //MARK: - Methods
+    func text(for index: Int) -> some View {
+        let title = colorKeys[index]
+        
+        return Text(title)
+            .background(colors[colorKeys[index]])
     }
 }
 
