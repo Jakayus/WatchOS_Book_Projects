521636b7265748aa90e14eaed59ce5c61555301c
Joel Sereno <jakayus123@gmail.com>
2021-12-18 21:47:27 -0700

    Add examples as part of technique project

--- Project6/Project6 WatchKit Extension/ContentView.swift
+++ Project6/Project6 WatchKit Extension/ContentView.swift
@@ -8,9 +8,42 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var useRedText = false
+    
     var body: some View {
-        Text("Hello, World!")
-            .padding()
+        
+        //example 1
+//        Text("Hello, World!")
+//            .frame(maxWidth: .infinity, maxHeight: .infinity)
+//            .background(Color.red)
+//            .ignoresSafeArea()
+        
+        //example 2
+//        Button("Hello World") {
+//            print(type(of: self.body))
+//        }
+//        .frame(width: 150)
+//        .background(Color.red)
+        
+        //example 3
+//        Text("Hello World")
+//            .padding()
+//            .background(Color.red)
+//            .padding()
+//            .background(Color.blue)
+//            .padding()
+//            .background(Color.green)
+//            .padding()
+//            .background(Color.yellow)
+        
+        Button("Press Me") {
+            //flip the Boolean between true and false
+            self.useRedText.toggle()
+        }
+        .foregroundColor(useRedText ? .red : .blue)
+        
+        
     }
 }
 
