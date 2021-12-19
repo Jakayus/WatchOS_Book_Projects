c534f4c5ffff8c7cef510a4d95b81376c7988b72
Joel Sereno <jakayus123@gmail.com>
2021-12-19 11:24:20 -0700

    Project 7 setup

---
+++ Project7/Project7 WatchKit Extension/Project7App.swift
@@ -0,0 +1,19 @@
+//
+//  Project7App.swift
+//  Project7 WatchKit Extension
+//
+//  Created by Joel Sereno on 12/19/21.
+//
+
+import SwiftUI
+
+@main
+struct Project7App: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
---
+++ Project7/Project7Tests/Project7Tests.swift
@@ -0,0 +1,33 @@
+//
+//  Project7Tests.swift
+//  Project7Tests
+//
+//  Created by Joel Sereno on 12/19/21.
+//
+
+import XCTest
+@testable import Project7_WatchKit_Extension
+
+class Project7Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
