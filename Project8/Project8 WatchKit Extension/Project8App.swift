401b7e2b9ffb3f749a0e5fe6a2fdd533bafb9f3b
Joel Sereno <jakayus123@gmail.com>
2021-12-26 16:28:33 -0700

    Project 8 Setup

---
+++ Project8/Project8 WatchKit Extension/Project8App.swift
@@ -0,0 +1,19 @@
+//
+//  Project8App.swift
+//  Project8 WatchKit Extension
+//
+//  Created by Joel Sereno on 12/26/21.
+//
+
+import SwiftUI
+
+@main
+struct Project8App: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
---
+++ Project8/Project8UITests/Project8UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Project8UITestsLaunchTests.swift
+//  Project8UITests
+//
+//  Created by Joel Sereno on 12/26/21.
+//
+
+import XCTest
+
+class Project8UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
