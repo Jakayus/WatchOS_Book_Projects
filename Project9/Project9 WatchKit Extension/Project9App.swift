838b42cbc2483ddd6b11f3f6cbe15335f981f1f8
Joel Sereno <jakayus123@gmail.com>
2021-12-29 09:15:31 -0700

    Project 9 initial setup

---
+++ Project9/Project9 WatchKit Extension/Project9App.swift
@@ -0,0 +1,19 @@
+//
+//  Project9App.swift
+//  Project9 WatchKit Extension
+//
+//  Created by Joel Sereno on 12/29/21.
+//
+
+import SwiftUI
+
+@main
+struct Project9App: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
