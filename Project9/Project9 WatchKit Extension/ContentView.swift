d05651f4c6904e51dbcaf293ba3d42754ef0fbba
Joel Sereno <jakayus123@gmail.com>
2021-12-29 09:31:33 -0700

    Add Implicit Animation example
    NOTE: .animation(.default) depracated in watchOS 8, had to use .animation(.default, value: animationAmount) instead, where animationAmount was a @State CGFloat created at the top of the file used for animations

--- Project9/Project9 WatchKit Extension/ContentView.swift
+++ Project9/Project9 WatchKit Extension/ContentView.swift
@@ -8,9 +8,21 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var animationAmount: CGFloat = 1
+    
     var body: some View {
-        Text("Hello, World!")
-            .padding()
+        Button("Tap Me") {
+            animationAmount += 0.25
+        }
+        .buttonStyle(PlainButtonStyle())
+        .padding(50)
+        .background(Color.red)
+        .foregroundColor(.white)
+        .clipShape(Circle())
+        .scaleEffect(animationAmount)
+        .blur(radius: (animationAmount - 1) * 3)
+        .animation(.default, value: animationAmount) // replaces .animation(.default)
     }
 }
 
