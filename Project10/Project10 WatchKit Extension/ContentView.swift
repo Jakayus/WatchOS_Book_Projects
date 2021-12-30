dd2bdca2524d427d1db011431ebf0b30ed6d7277
Joel Sereno <jakayus123@gmail.com>
2021-12-30 16:58:12 -0700

    Project 10 Initial Setup

---
+++ Project10/Project10 WatchKit Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Project10 WatchKit Extension
+//
+//  Created by Joel Sereno on 12/30/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Project10/Project10 WatchKit Extension/Project10App.swift
@@ -0,0 +1,19 @@
+//
+//  Project10App.swift
+//  Project10 WatchKit Extension
+//
+//  Created by Joel Sereno on 12/30/21.
+//
+
+import SwiftUI
+
+@main
+struct Project10App: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
