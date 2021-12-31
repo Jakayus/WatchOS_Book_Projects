00d0f9c90f89ae5d2ef6b1a3dfb4cced18b0f8d8
Joel Sereno <jakayus123@gmail.com>
2021-12-30 17:51:53 -0700

    Add user interface basics

--- Project10/Project10 WatchKit Extension/ContentView.swift
+++ Project10/Project10 WatchKit Extension/ContentView.swift
@@ -6,11 +6,36 @@
 //
 
 import SwiftUI
+import HealthKit
 
 struct ContentView: View {
+    //MARK: - Properties
+    let activities: [(name: String, type: HKWorkoutActivityType)] = [
+        ("Cycling", .cycling),
+        ("Running", .running),
+        ("Wheelchair", .wheelchairRunPace)
+    
+    ]
+    
+    @State private var selectedActivity = 0
+    
+    
+    
+    //MARK: - View
     var body: some View {
-        Text("Hello, World!")
-            .padding()
+        VStack {
+            Picker("Choose an activity", selection: $selectedActivity) {
+                ForEach(0..<activities.count) { index in
+                    Text(activities[index].name)
+                }
+            }
+            
+            Button("Start Workout") {
+                guard HKHealthStore.isHealthDataAvailable() else { return }
+            }
+            
+            
+        }//end VStack
     }
 }
 
---
+++ Project10/Project10 WatchKit Extension/WorkoutView.swift
@@ -0,0 +1,21 @@
+//
+//  WorkoutView.swift
+//  Project10 WatchKit Extension
+//
+//  Created by Joel Sereno on 12/30/21.
+//
+
+import SwiftUI
+
+struct WorkoutView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .padding()
+    }
+}
+
+struct WorkoutView_Previews: PreviewProvider {
+    static var previews: some View {
+        WorkoutView()
+    }
+}
