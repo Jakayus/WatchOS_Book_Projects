49ca9a0307c634dd4f7fd1e1818ff45c9f6e89b9
Joel Sereno <jakayus123@gmail.com>
2022-01-02 22:35:14 -0700

    Create initial Spritekit functionality (proof of concept)

---
+++ Project11/Project11 WatchKit Extension/GameScene.swift
@@ -0,0 +1,17 @@
+//
+//  GameScene.swift
+//  Project11 WatchKit Extension
+//
+//  Created by Joel Sereno on 1/2/22.
+//
+
+import SpriteKit
+import WatchKit
+
+class GameScene: SKScene {
+
+    override func sceneDidLoad() {
+        backgroundColor = .blue
+    }
+    
+}
--- Project11/Project11 WatchKit Extension/InterfaceController.swift
+++ Project11/Project11 WatchKit Extension/InterfaceController.swift
@@ -10,9 +10,17 @@ import Foundation
 
 
 class InterfaceController: WKInterfaceController {
-
+    
+    var gameScene: GameScene!
+    
+    @IBOutlet weak var gameInterface: WKInterfaceSKScene! //outlets are properties, and actions are methods
+    
     override func awake(withContext context: Any?) {
         // Configure interface objects here.
+        
+        //WatchKit equivalent of SwiftUI's onAppear
+        
+        startGame(self) //this mean scurrent screen is making the method call
     }
     
     override func willActivate() {
@@ -23,4 +31,13 @@ class InterfaceController: WKInterfaceController {
         // This method is called when watch view controller is no longer visible
     }
 
+    @IBAction func startGame(_ sender: Any) {
+        
+        gameScene = GameScene()
+        gameScene.scaleMode = .resizeFill //allows design to scale across all sizes
+        gameScene.anchorPoint = CGPoint(x: 0.5, y: 0.5) //center
+        
+        gameInterface.presentScene(gameScene) // show scene immediately
+        
+    }
 }
