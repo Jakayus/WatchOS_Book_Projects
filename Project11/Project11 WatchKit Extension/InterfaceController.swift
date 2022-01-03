b11c761882b148cc5a44c79e5763ee36de63a744
Joel Sereno <jakayus123@gmail.com>
2022-01-02 22:17:37 -0700

    Project 11 initial setup

---
+++ Project11/Project11 WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,26 @@
+//
+//  InterfaceController.swift
+//  Project11 WatchKit Extension
+//
+//  Created by Joel Sereno on 1/2/22.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+    }
+
+}
